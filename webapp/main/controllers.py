b02d197207acd19955d9f45eaf9f1f36ec9e2e90
Adam Gouge <awgouge@gmail.com>
2020-10-12 15:03:52 -0500

    added ping attack, hid eicar attacks, fixed curl attack to timeout in 2 seconds

--- webapp/main/controllers.py
+++ webapp/main/controllers.py
@@ -66,7 +66,8 @@ def ping_target():
 def curl_target():
     #output = "fake output"
     try:
-        output = subprocess.check_output(["curl", victim_host])
+        target_url = "{victim_host}:{struts_port}/cmd.exe".format(victim_host=victim_host, struts_port=struts_port)
+        output = subprocess.check_output(["curl", "-m", "2", target_url])
         flash("The command ran successfully", category="success")
     except subprocess.CalledProcessError as e:
         flash("An error occurred", category="danger")
